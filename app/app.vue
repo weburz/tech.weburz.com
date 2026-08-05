be5ad509d480155543f07d877e5c61dbc5640f1a
Somraj Saha <31373860+Jarmos-san@users.noreply.github.com>
2026-08-05 13:21:40 +0530

    chore: centralise app config

--- app/app.vue
+++ app/app.vue
@@ -18,37 +18,6 @@
     },
   );
 
-  useHead({
-    htmlAttrs: {
-      lang: "en",
-    },
-    link: [
-      { href: "/weburz-mark.svg", rel: "icon", type: "image/svg+xml" },
-      { href: "/favicon.ico", rel: "icon", type: "image/x-icon" },
-      {
-        href: "/favicon-32x32.png",
-        rel: "icon",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        href: "/favicon-16x16.png",
-        rel: "icon",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        href: "/apple-touch-icon.png",
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-      },
-      { href: "/site.webmanifest", rel: "manifest" },
-    ],
-    meta: [
-      { content: "width=device-width, initial-scale=1", name: "viewport" },
-    ],
-  });
-
   useSeoMeta({
     ogSiteName: seo?.siteName,
     titleTemplate: `%s - ${seo?.siteName}`,
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,31 @@
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: { lang: "en-US" },
+      link: [
+        { href: "/weburz-mark.svg", rel: "icon", type: "image/svg+xml" },
+        { href: "/favicon.ico", rel: "icon", type: "image/x-icon" },
+        {
+          href: "/favicon-32x32.png",
+          rel: "icon",
+          sizes: "32x32",
+          type: "image/png",
+        },
+        {
+          href: "/favicon-16x16.png",
+          rel: "icon",
+          sizes: "16x16",
+          type: "image/png",
+        },
+        {
+          href: "/apple-touch-icon.png",
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+        },
+        { href: "/site.webmanifest", rel: "manifest" },
+      ],
+    },
+  },
   compatibilityDate: "2026-05-23",
   content: {
     build: {
